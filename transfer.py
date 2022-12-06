fec8f7b7f7d4928d78cad465461bb352f2fb5a43
fernangit <morio.tsunaki@gmail.com>
2022-12-06 22:42:15 +0900

    modify

--- transfer.py
+++ transfer.py
@@ -1,4 +1,5 @@
 import pyautogui
+import time
 
 a = 'あかさたなはまやらわがざだばぁゃアカサタナハマヤラワガザダバァャ'
 i = 'いきしちにひみりぎじぢびぃイキシチニヒミリギジヂビィ'
@@ -15,25 +16,32 @@ def transferUtterance(utterance):
         if utt in a:
             print('あ')
             pyautogui.hotkey('a')
+            time.sleep(0.2)
         elif utt in i:
             print('い')
             pyautogui.hotkey('i')
+            time.sleep(0.2)
         elif utt in u:
             print('う')
             pyautogui.hotkey('u')
+            time.sleep(0.2)
         elif utt in e:
             print('え')
             pyautogui.hotkey('e')
+            time.sleep(0.2)
         elif utt in o:
             print('お')
             pyautogui.hotkey('o')
+            time.sleep(0.2)
         elif utt in n:
             print('ん')
             pyautogui.hotkey('n')
+            time.sleep(0.2)
         else:
             #一つ前の音にする
             transferUtterance(butt)
             utt = butt
+            time.sleep(0.2)
         butt = utt
 
 if __name__ == "__main__":
