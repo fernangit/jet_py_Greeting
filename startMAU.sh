a115d803f2ac8af1507beb9bae6850d6d1fcef02
fernangit <morio.tsunaki@gmail.com>
2023-02-17 15:23:46 +0900

    modify

--- startMAU.sh
+++ startMAU.sh
@@ -1,16 +1,19 @@
 #サーバ起動
+cd /home/jetson/work
 gnome-terminal -- sh startServer.sh
 sleep 10
 #ブラウザ起動
+cd /home/jetson/work
 gnome-terminal -- sh startBrowser.sh
 sleep 60
 #Greeting起動
+cd /home/jetson/work
 gnome-terminal -- sh startGreeting.sh
 sleep 300
 #ブラウザを最前面に移動
 browser_window_id=$(xdotool search --name --onlyvisible "chromium")
 xdotool windowactivate $browser_window_id
-sleep 5
+sleep 10
 #Unityを最大表示
 xdotool key z
 
