8e253f01ac716102b0991dc90950fece11cf09b0
fernangit <morio.tsunaki@gmail.com>
2023-03-21 17:48:21 +0900

    modify

--- startServer.sh
+++ startServer.sh
@@ -1,9 +1,12 @@
 rand=$RANDOM
 if [ $((rand % 100)) -lt 20 ]; then
+  echo 'webGL_Greeting2'
   cd /home/jetson/work/webGL_Greeting2/WebGL
 elif [ $((rand % 100)) -lt 30 ]; then
+  echo 'webGL_Greeting3'
   cd /home/jetson/work/webGL_Greeting3/WebGL
 else
+  echo 'webGL_Greeting1'
   cd /home/jetson/work/webGL_Greeting1/WebGL
 fi
 python3 startServer.py
