dce3142e8e08fef0c2f3a2cb4297dba100ab12d3
fernangit <morio.tsunaki@gmail.com>
2023-04-08 22:50:08 +0900

    modify sh

--- startServer.sh
+++ startServer.sh
@@ -1,9 +1,9 @@
 rand=$(od -vAn -N4 -tu4 < /dev/random)
 echo "$rand"
-if [ $((rand % 100)) -lt 20 ]; then
+if [ $((rand % 100)) -lt 40 ]; then
   echo 'webGL_Greeting2'
   cd /home/jetson/work/webGL_Greeting2/WebGL
-elif [ $((rand % 100)) -lt 30 ]; then
+elif [ $((rand % 100)) -lt 60 ]; then
   echo 'webGL_Greeting3'
   cd /home/jetson/work/webGL_Greeting3/WebGL
 else
